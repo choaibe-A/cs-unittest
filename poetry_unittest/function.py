681e949ed6cc30542f5b330d5cd053f17fb4fb98
choaibe-A <cbaine.baine@gmail.com>
2023-11-01 04:51:37 +0100

    (fix): updating python code to meet pylint suggestions

--- poetry_unittest/function.py
+++ poetry_unittest/function.py
@@ -1,17 +1,25 @@
+"""
+This module contains functions for sorting items by frequency.
+"""
 from collections import Counter
-from typing import Iterable
 
 def frequency_sort(items):
+    """
+    Sorts a list of items by their frequency and original order of appearance.
+
+    :param items: A list of items to be sorted.
+    :return: A list of items sorted by frequency.
+    """
     # Use Counter to count the frequency of each element
     item_counts = Counter(items)
-    
     # Define a custom sorting key that sorts first by frequency (in reverse order)
     # and then by the original order of appearance
     def custom_sort_key(item):
         return (-item_counts[item], items.index(item))
-    
     # Sort the items using the custom key
-    sorted_items = sorted(items, key=custom_sort_key)
-    
+    sorted_items = sorted(items,key=custom_sort_key)
     return sorted_items
+
+if __name__ == '__main__':
+    frequency_sort(1)
 
--- tests/test.py
+++ tests/test.py
@@ -1,41 +1,58 @@
-import pytest
+'''import modules'''
+
 import sys
 import os
+import pytest
+from poetry_unittest.function import frequency_sort
 
 # Get the current script's directory and add the parent directory (project root) to sys.path
 script_dir = os.path.dirname(os.path.realpath(__file__))
 project_root = os.path.abspath(os.path.join(script_dir, os.pardir))
 sys.path.append(project_root)
 
-from poetry_unittest.function import frequency_sort
+
 # Test cases
 
 def test_integer_sort():
+    '''
+    test frequency_sort for integers
+    '''
     input_list = [4, 6, 2, 2, 6, 4, 4, 4]
     expected_result = [4, 4, 4, 4, 6, 6, 2, 2]
     assert frequency_sort(input_list) == expected_result
 
 def test_string_sort():
+    '''
+    test frequency_sort for strings
+    '''
     input_list = ["bob", "bob", "carl", "alex", "bob"]
     expected_result = ["bob", "bob", "bob", "carl", "alex"]
     assert frequency_sort(input_list) == expected_result
 
 def test_mixed_sort():
+    '''
+    test frequency_sort for mixed types
+    '''
     input_list = [4, "bob", "carl", "bob", 4, "alex", 4]
     expected_result = [4, 4, 4, "bob", "bob", "carl", "alex"]
     assert frequency_sort(input_list) == expected_result
 
 def test_empty_list():
+    '''
+    test frequency_sort for empty list
+    '''
     input_list = []
     expected_result = []
     assert frequency_sort(input_list) == expected_result
 
 def test_single_element():
+    '''
+    test frequency_sort for single element
+    '''
     input_list = [42]
     expected_result = [42]
     assert frequency_sort(input_list) == expected_result
 
 # Run tests with pytest
-#if __name__ == "__main__":
-#    pytest.main()
-
+if __name__ == "__main__":
+    pytest.main()
